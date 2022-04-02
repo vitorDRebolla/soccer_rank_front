c0a5faabc81cc539b07407ae9e13444afa27478c
Vitor Rebolla <vitor_rebolla@hotmail.com>
2022-04-02 20:41:49 -0300

    axios and bootstrap

---
+++ src/components/TeamsTable.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="d-flex justify-content-center mt-5">
+    <table class="table">
+      
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TeamsTable',
+  data() {
+    return {
+      teams: []
+    }
+  },
+  methods: {
+    getTeams() {
+      this.$axios.get('/api/teams').then(({ data }) => {
+        this.teams = data;
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css"
 import { createApp } from 'vue';
 import App from './App.vue';
 import api from './configs/axios';
@@ -6,3 +7,5 @@ const app = createApp(App);
 
 app.config.globalProperties.$axios = api;
 app.mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js"
