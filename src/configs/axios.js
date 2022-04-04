676c4c6e8938d1f364d13a85725f758513ebdfcf
Vitor Rebolla <vitor_rebolla@hotmail.com>
2022-04-04 02:04:58 -0300

    changing the local server

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://campeonato-brasileiro.herokuapp.com/',
+    baseURL: 'http://localhost:8000/',
     timeout: 5000
 });
 
